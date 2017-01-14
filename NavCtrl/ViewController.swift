762252e0508f5cbcbf0ed209b37ad87b9f1dd621
timothyjhull <timothyjhull@gmail.com>
2017-01-14 13:04:06 -0500

    Initial Commit

---
+++ NavCtrl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavCtrl
+//
+//  Created by Timothy Hull on 1/14/17.
+//  Copyright © 2017 Sponti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
